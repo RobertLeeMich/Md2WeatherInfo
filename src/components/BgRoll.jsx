01ed400bdcd53003091721a8fbcc90b0185a20e3
Robert Michaud <guildwarsguy123@yahoo.com>
2023-08-24 13:20:49 -0400

    reverting

--- src/components/BgRoll.jsx
+++ src/components/BgRoll.jsx
@@ -4,18 +4,18 @@ function BgRoll({ sunrise, sunset }) {
   useEffect(() => {
     const updateBackground = () => {
       const now = new Date();
-      const currentTime = now.getTime() / 1000; // Convert to Unix timestamp
+      const currentTime = now.getTime() / 1000;
       const position = ((currentTime - sunrise) / (sunset - sunrise)) * 100;
       document.body.style.backgroundPosition = `${position}% 0%`;
     };
 
-    updateBackground(); // Update the background immediately
-    const intervalId = setInterval(updateBackground, 60000); // Update the background every minute
+    updateBackground(); 
+    const intervalId = setInterval(updateBackground, 60000);
 
-    return () => clearInterval(intervalId); // Clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
   }, [sunrise, sunset]);
 
-  return null; // This component doesn't render anything visible
+  return null;
 }
 
 export default BgRoll;
